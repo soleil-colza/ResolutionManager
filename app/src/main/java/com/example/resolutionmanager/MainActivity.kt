11fc7618f0a76d3657451bc100decd158164b4c2
Hina Nakahira <soleil.colza@icloud.com>
2024-03-02 02:32:03 +0900

    [add]ResolutionsScreenTitle

--- app/src/main/java/com/example/resolutionmanager/MainActivity.kt
+++ app/src/main/java/com/example/resolutionmanager/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.example.resolutionmanager
 
 import android.os.Bundle
+import android.R
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.stringResource
 import com.example.resolutionmanager.ui.theme.ResolutionManagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,9 +23,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ResolutionManagerTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
                 }
             }
         }
@@ -27,17 +31,19 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ResolutionManagerApp(){
+
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ResolutionManagerTheme {
-        Greeting("Android")
+fun ResolutionScreen() {
+    Text(text = "Resolution Screen")
+}
+
+@Composable
+fun ResolutionsScreenTitle() {
+    Row(){
+        Icon(imageVector = Icons.Default.Person, contentDescription = null)
+        Text(text = stringResource(com.example.resolutionmanager.R.string.resolutions))
     }
 }
